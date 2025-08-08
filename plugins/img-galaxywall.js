68cc442969a1423b9ad44753bba562596eb32220
Carlos <supernumerovirtual@gmail.com>
2025-08-08 13:58:34 -0400

    Update img-galaxywall.js

--- plugins/img-galaxywall.js
+++ plugins/img-galaxywall.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 
-const thumbnailUrl = 'https://qu.ax/galaxywall.jpg'; // Miniatura cósmica y envolvente
+const thumbnailUrl = 'https://qu.ax/MvYPM.jpg'; // Miniatura cósmica y envolvente
 
 const contextInfo = {
     externalAdReply: {
