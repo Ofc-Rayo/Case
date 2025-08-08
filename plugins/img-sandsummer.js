347e6e71e87cf2e632f087ffec1ea46e023ad410
Carlos <supernumerovirtual@gmail.com>
2025-08-08 13:57:44 -0400

    Update img-sandsummer.js

--- plugins/img-sandsummer.js
+++ plugins/img-sandsummer.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 
-const thumbnailUrl = 'https://qu.ax/sandsummer.jpg'; // Imagen evocadora de verano y arena
+const thumbnailUrl = 'https://qu.ax/MvYPM.jpg'; // Imagen evocadora de verano y arena
 
 const contextInfo = {
     externalAdReply: {
