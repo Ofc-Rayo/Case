d9004a47f1713e76c48a4ffd54efff3e56006fd0
Carlos <supernumerovirtual@gmail.com>
2025-08-26 20:35:02 -0400

    Actualizar fun-leandro.js

--- plugins/fun-leandro.js
+++ plugins/fun-leandro.js
@@ -3,7 +3,7 @@ async function handler(conn, { message }) {
   const quoted = message;
 
   try {
-    const imageUrl = 'https://raw.githubusercontent.com/Kone457/Nexus/refs/heads/main/Cenix.jpg';
+    const imageUrl = 'https://raw.githubusercontent.com/Kone457/Nexus/refs/heads/main/leandrita.jpg';
 
     const contextInfo = {
       externalAdReply: {
