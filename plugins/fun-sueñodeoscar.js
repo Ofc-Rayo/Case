3e80685ace0ced7ae90cfa0f5750e043f6d483e4
Carlos <supernumerovirtual@gmail.com>
2025-08-26 23:52:33 -0400

    Crear fun-sueñodeoscar.js

---
+++ plugins/fun-sueñodeoscar.js
@@ -0,0 +1,50 @@
+async function handler(conn, { message }) {
+  const jid = message.key.remoteJid;
+  const quoted = message;
+
+  try {
+    const imageUrl = 'https://qu.ax/GNebj.jpg';
+
+    const contextInfo = {
+      externalAdReply: {
+        title: '🤤 Oscar - soñando',
+        body: 'Una imagen que susurra desde el otro lado...',
+        mediaType: 1,
+        previewType: 0,
+        sourceUrl: imageUrl,
+        thumbnailUrl: 'https://qu.ax/MvYPM.jpg'
+      }
+    };
+
+    const caption = `
+🤤 *Suena Oscar...*\n> Pero recuerda, ella no te quiere 😈
+`.trim();
+
+    await conn.sendMessage(jid, {
+      image: { url: imageUrl },
+      caption,
+      contextInfo
+    }, { quoted });
+
+  } catch (err) {
+    console.error('💥 Error en el comando Cenix:', err.message);
+    await conn.sendMessage(jid, {
+      text: '*⚠️ No se pudo enviar la imagen.*\n\n> 🧵 El hilo visual se ha enredado...',
+      contextInfo: {
+        externalAdReply: {
+          title: 🤤 Oscar - en sus sueños humedos',
+          body: 'Error en la conexión estética...',
+          mediaType: 1,
+          previewType: 0,
+          sourceUrl: imageUrl,
+          thumbnailUrl: 'https://qu.ax/MvYPM.jpg'
+        }
+      }
+    }, { quoted });
+  }
+}
+
+module.exports = {
+  command: 'sueñodeoscar',
+  handler
+};
