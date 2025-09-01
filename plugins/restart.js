f0e676f4c3757616c6f40e708fdd5b6f8966a102
Ofc-Rayo <ivandsow@gmail.com>
2025-09-01 15:53:45 -0300

    Crear restart.js

---
+++ plugins/restart.js
@@ -0,0 +1,16 @@
+const handler = async (m, { conn, usedPrefix, command }) => {
+    try {
+        m.reply('「🔁」 Reiniciando El Bot....');
+        setTimeout(() => {
+            process.exit(0);
+        }, 3000); 
+    } catch (error) {
+        console.log(error);
+        conn.reply(m.chat, `${error}`, m);
+    }
+};
+
+module.exports = {
+    command: 'claude',
+    handler,
+};
