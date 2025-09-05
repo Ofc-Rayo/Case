08eda6a62811829b73369b420e7710a7f64975ed
Ofc-Rayo <ivandsow@gmail.com>
2025-09-05 15:29:16 -0300

    Actualizar Ai-hd.js

--- plugins/Ai-hd.js
+++ plugins/Ai-hd.js
@@ -1,1 +1,33 @@
-///
+const cp = require('child_process');
+const { promisify } = require('util');
+
+const exec = promisify(cp.exec).bind(cp);
+
+module.exports = {
+  command: "$",
+  customPrefix: /^[$] /, // activa solo si el mensaje comienza con $
+  rowner: true, // solo el dueño real puede usar este comando
+  help: ["$"],
+  tags: ["owner"],
+  
+  handler: async function (m, { conn, isOwner, command, text }) {
+    if (!isOwner) return;
+
+    let result;
+    try {
+      result = await exec(command.trimStart() + ' ' + text.trimEnd());
+    } catch (e) {
+      result = e;
+    }
+
+    const { stdout, stderr } = result;
+
+    if (stdout?.trim()) {
+      await conn.reply(m.chat, stdout, m);
+    }
+
+    if (stderr?.trim()) {
+      await conn.reply(m.chat, stderr, m);
+    }
+  }
+};
